f4610d4826c3e7fbe08b57b956f82f6a50a38586
wenhx8 <wenhuaixiang@126.com>
2021-07-06 21:14:30 +0800

    Add files via upload

---
+++ build_container_common.sh
@@ -0,0 +1,118 @@
+#!/bin/bash -e
+
+function download_and_check () {
+  local to=$1
+  local url=$2
+  local sha256=$3
+
+  curl -fsSL --output "${to}" "${url}"
+  echo "${sha256}  ${to}" | sha256sum --check
+}
+
+function install_gn(){
+  # Install gn tools which will be used for building wee8
+  if [[ "$(uname -m)" == "x86_64" ]]; then
+    wget -O gntool.zip https://chrome-infra-packages.appspot.com/dl/gn/gn/linux-amd64/+/latest
+    unzip gntool.zip -d gntool
+    cp gntool/gn /usr/local/bin/gn
+    chmod +x /usr/local/bin/gn
+    rm -rf gntool*
+  elif [[ "$(uname -m)" == "aarch64" ]]; then
+    # install gn tools
+    download_and_check /usr/local/bin/gn https://github.com/envoyproxy/envoy-build-tools/releases/download/build-tools/gn-arm64 \
+      37f2960d488251760c56683dcf2cc4dfb2c2c13af476f86475eee206fafe21e2
+    chmod +x /usr/local/bin/gn
+  fi
+}
+
+if [[ "$(uname -m)" == "x86_64" ]]; then
+  # buildifier
+  VERSION=2.2.1
+  download_and_check /usr/local/bin/buildifier https://github.com/bazelbuild/buildtools/releases/download/"$VERSION"/buildifier \
+    731a6a9bf8fca8a00a165cd5b3fbac9907a7cf422ec9c2f206b0a76c0a7e3d62
+  chmod +x /usr/local/bin/buildifier
+
+  # buildozer
+  VERSION=2.2.1
+  download_and_check /usr/local/bin/buildozer https://github.com/bazelbuild/buildtools/releases/download/"$VERSION"/buildozer \
+    5aa4f70f5f04599da2bb5b7e6a46af3e323a3a744c11d7802517d956909633ae
+  chmod +x /usr/local/bin/buildozer
+
+  # bazelisk
+  VERSION=1.7.4
+  download_and_check /usr/local/bin/bazel https://github.com/bazelbuild/bazelisk/releases/download/v${VERSION}/bazelisk-linux-amd64 \
+    ab258203db518a54cbd5afa80864d5a3bb366058b95e7a7df4134b0b7765a378
+  chmod +x /usr/local/bin/bazel
+fi
+
+if [[ "$(uname -m)" == "aarch64" ]]; then
+  # bazelisk
+  VERSION=1.7.4
+  download_and_check /usr/local/bin/bazel https://github.com/bazelbuild/bazelisk/releases/download/v${VERSION}/bazelisk-linux-arm64 \
+    aea0ff1036bd4c3703c5c10d07a059d885f2f6ad2f36c2175fc45a1f774ee341
+  chmod +x /usr/local/bin/bazel
+fi
+
+LLVM_RELEASE="clang+llvm-${LLVM_VERSION}-${LLVM_DISTRO}"
+LLVM_DOWNLOAD_PREFIX=${LLVM_DOWNLOAD_PREFIX:-https://github.com/llvm/llvm-project/releases/download/llvmorg-}
+download_and_check "${LLVM_RELEASE}.tar.xz" "${LLVM_DOWNLOAD_PREFIX}${LLVM_VERSION}/${LLVM_RELEASE}.tar.xz" "${LLVM_SHA256SUM}"
+tar Jxf "${LLVM_RELEASE}.tar.xz"
+mv "./${LLVM_RELEASE}" /opt/llvm
+chown -R root:root /opt/llvm
+rm "./${LLVM_RELEASE}.tar.xz"
+echo "/opt/llvm/lib" > /etc/ld.so.conf.d/llvm.conf
+ldconfig
+
+# Install gn tools.
+install_gn
+
+# Install lcov
+LCOV_VERSION=1.14
+download_and_check lcov-${LCOV_VERSION}.tar.gz https://github.com/linux-test-project/lcov/releases/download/v${LCOV_VERSION}/lcov-${LCOV_VERSION}.tar.gz \
+  14995699187440e0ae4da57fe3a64adc0a3c5cf14feab971f8db38fb7d8f071a
+tar zxf lcov-${LCOV_VERSION}.tar.gz
+make -C lcov-${LCOV_VERSION} install
+rm -rf "lcov-${LCOV_VERSION}" "./lcov-${LCOV_VERSION}.tar.gz"
+
+
+# Install sanitizer instrumented libc++, skipping for architectures other than x86_64 for now.
+if [[ "$(uname -m)" != "x86_64" ]]; then
+  exit 0
+fi
+
+export PATH="/opt/llvm/bin:${PATH}"
+
+WORKDIR=$(mktemp -d)
+function cleanup {
+  rm -rf "${WORKDIR}"
+}
+
+trap cleanup EXIT
+pushd "${WORKDIR}"
+curl -sSfL "https://github.com/llvm/llvm-project/archive/llvmorg-${LLVM_VERSION}.tar.gz" | tar zx
+
+function install_libcxx() {
+  local LLVM_USE_SANITIZER=$1
+  local LIBCXX_PATH=$2
+
+  mkdir "${LIBCXX_PATH}"
+  pushd "${LIBCXX_PATH}"
+
+  cmake -GNinja -DLLVM_ENABLE_PROJECTS="libcxxabi;libcxx" -DLLVM_USE_LINKER=lld -DLLVM_USE_SANITIZER=${LLVM_USE_SANITIZER} -DCMAKE_BUILD_TYPE=RelWithDebInfo \
+    -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_INSTALL_PREFIX="/opt/libcxx_${LIBCXX_PATH}" "../llvm-project-llvmorg-${LLVM_VERSION}/llvm"
+  ninja install-cxx install-cxxabi
+
+  if [[ ! -z "$(diff -r /opt/libcxx_${LIBCXX_PATH}/include/c++ /opt/llvm/include/c++)" ]]; then
+    echo "Different libc++ is installed";
+    exit 1
+  fi
+
+  rm -rf "/opt/libcxx_${LIBCXX_PATH}/include"
+
+  popd
+}
+
+install_libcxx MemoryWithOrigins msan
+install_libcxx Thread tsan
+
+popd
---
+++ build_container_ubuntu.sh
@@ -0,0 +1,159 @@
+#!/bin/bash
+
+set -e
+
+ARCH="$(uname -m)"
+
+# Setup basic requirements and install them.
+export DEBIAN_FRONTEND=noninteractive
+apt-get update -y
+apt-get install -y --no-install-recommends locales software-properties-common apt-transport-https curl gpg-agent
+
+# set locale
+localedef -i en_US -c -f UTF-8 -A /usr/share/locale/locale.alias en_US.UTF-8
+
+# gcc-9
+add-apt-repository -y ppa:ubuntu-toolchain-r/test
+apt-get update
+apt-get install -y --no-install-recommends g++-9
+update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-9 1000
+update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-9 1000
+update-alternatives --config gcc
+update-alternatives --config g++
+
+# Google Cloud SDK
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] http://packages.cloud.google.com/apt cloud-sdk main" \
+  | tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | \
+  apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
+
+# docker-ce-cli
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+case $ARCH in
+    'ppc64le' )
+        add-apt-repository "deb [arch=ppc64le] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+        ;;
+    'x86_64' )
+        add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+        ;;
+    'aarch64' )
+        add-apt-repository "deb [arch=arm64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+        ;;
+esac
+
+# CMake
+curl -fsSL https://apt.kitware.com/keys/kitware-archive-latest.asc | apt-key add -
+apt-add-repository "deb https://apt.kitware.com/ubuntu/ $(lsb_release -cs) main"
+
+apt-get update -y
+
+PACKAGES=(
+    automake
+    bc
+    bzip2
+    cmake
+    docker-ce-cli
+    doxygen
+    gdb
+    git
+    google-cloud-sdk
+    graphviz
+    jq
+    libncurses-dev
+    libtool
+    make
+    ninja-build
+    patch
+    python
+    python-pip
+    python-setuptools
+    python3
+    python3-pip
+    python3-setuptools
+    python3.8
+    rsync
+    ssh-client
+    strace
+    sudo
+    tcpdump
+    time
+    tshark
+    unzip
+    wget
+    xz-utils
+    zip)
+
+# todo: remove this once kitware repo is fixed
+if [[ "$ARCH" == "aarch64" ]]; then
+    PACKAGES+=("cmake-data=3.10.2-1ubuntu2.18.04.1")
+else
+    PACKAGES+=("cmake-data")
+fi
+
+apt-get install -y --no-install-recommends "${PACKAGES[@]}"
+
+# Set LLVM version for each cpu architecture.
+LLVM_VERSION=11.0.1
+case $ARCH in
+    'ppc64le' )
+        LLVM_DISTRO=powerpc64le-linux-ubuntu-18.04
+        LLVM_SHA256SUM=a60a35f6c9f280268df8afe76f4a5349426f8b8eefd40eb885eae80b6e3647d0
+        ;;
+    'x86_64' )
+        LLVM_DISTRO=x86_64-linux-gnu-ubuntu-16.04
+        LLVM_SHA256SUM=67f18660231d7dd09dc93502f712613247b7b4395e6f48c11226629b250b53c5
+        ;;
+    'aarch64' )
+        LLVM_DISTRO=aarch64-linux-gnu
+        LLVM_SHA256SUM=39b3d3e3b534e327d90c77045058e5fc924b1a81d349eac2be6fb80f4a0e40d4
+        ;;
+esac
+
+# Bazel and related dependencies.
+case $ARCH in
+    'ppc64le' )
+        BAZEL_LATEST="$(curl https://oplab9.parqtec.unicamp.br/pub/ppc64el/bazel/ubuntu_16.04/latest/ 2>&1 \
+          | sed -n 's/.*href="\([^"]*\).*/\1/p' | grep '^bazel' | head -n 1)"
+        curl -fSL https://oplab9.parqtec.unicamp.br/pub/ppc64el/bazel/ubuntu_16.04/latest/${BAZEL_LATEST} \
+          -o /usr/local/bin/bazel
+        chmod +x /usr/local/bin/bazel
+        ;;
+    'aarch64' )
+        if [ "$(lsb_release -cs)" == 'xenial' ]; then
+          apt install -y openjdk-8-jdk
+          apt install -y ca-certificates-java
+          update-ca-certificates -f
+        else
+          apt install -y openjdk-11-jdk
+        fi
+        ;;
+esac
+
+# additional apt installs
+apt-get install -y aspell expect
+rm -rf /var/lib/apt/lists/*
+
+# upstream install of shellcheck (taken from https://askubuntu.com/a/1228181)
+pushd /tmp || exit 1
+echo "c37d4f51e26ec8ab96b03d84af8c050548d7288a47f755ffb57706c6c458e027  /tmp/shellcheck-v0.7.0/shellcheck" > sc.checksum
+wget -qO- https://github.com/koalaman/shellcheck/releases/download/v0.7.0/shellcheck-v0.7.0.linux.x86_64.tar.xz | tar -xJf -
+sha256sum -c sc.checksum
+sudo cp shellcheck-v0.7.0/shellcheck /usr/local/bin
+rm -rf shellcheck-v0.7.0/ sc.checksum
+popd || exit 1
+
+# Setup tcpdump for non-root.
+groupadd -r pcap
+chgrp pcap /usr/sbin/tcpdump
+chmod 750 /usr/sbin/tcpdump
+setcap cap_net_raw,cap_net_admin=eip /usr/sbin/tcpdump
+
+# make python3.8 the default python3 interpreter
+update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8 1
+
+# virtualenv
+pip3 install -U virtualenv
+
+source ./build_container_common.sh
+
+apt-get clean
